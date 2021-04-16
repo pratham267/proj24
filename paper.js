c7555dc29114dabda63f08d75ba09040dec08c9e
pratham267 <81231649+pratham267@users.noreply.github.com>
2021-04-16 09:28:15 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,24 @@
+class paper{
+    constructor(x,y){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+        this.body=Bodies.circle(x,y,20,options)
+        World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rectMode(CENTER);
+        strokeWeight(3);
+        
+        fill(255,0,0);
+        ellipse(0,0,20);
+        pop();
+        
+        }
+}
